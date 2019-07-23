f063ce857435b54a116df51c4b1fe6999543813e
nishith-p <nishith.pinnawala@gmail.com>
2019-07-23 09:01:03 +0530

    Testing GitHub for the first time! :)

---
+++ app/src/main/java/com/example/student/lab2/Lab2MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.student.lab2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Lab2MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab2_main);
+    }
+}
